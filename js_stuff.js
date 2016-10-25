f11243d072816eb3a1ab46d4bebd1dd6fe8863be
Aadi Kalloo <aadi.kalloo@gmail.com>
2016-10-25 19:18:55 -0400

    Update 6-3
    Clean up code.

--- js_stuff.js
+++
@@ -1,6 +0,0 @@
-function sendMessage() {
-    var msg = document.getElementById("message_input").value;
-    socketio.emit("message_to_server", { message : msg});
-    var x = document.getElementById("box1").checked;
-    alert(x);
-}
